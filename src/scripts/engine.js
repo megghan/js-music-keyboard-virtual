d624425f737d8a37f48529b716a1ddae2c28f643
megghan <93269741+megghan@users.noreply.github.com>
2024-12-04 13:37:37 -0300

    minhas alterações

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -3,6 +3,7 @@ const volumeSlider = document.querySelector(".volume-slider input");
 const keysCheck = document.querySelector(".keys-check input");
 
 let mapedKeys = [];
+
 let audio = new Audio("src/tunes/a.wav");
 
 const playTune = (key) => {
@@ -20,20 +21,14 @@ pianoKeys.forEach((key) => {
   key.addEventListener("click", () => playTune(key.dataset.key));
   mapedKeys.push(key.dataset.key);
 });
-
 document.addEventListener("keydown", (e) => {
-  if (mapedKeys.includes(e.key)) {
-    playTune(e.key);
-  }
+  if (mapedKeys.includes(e.key)) {playTune(e.key);}
 });
 
-const handleVolume = (e) => {
-  audio.volume = e.target.value;
-};
 
-const showHideKeys = () => {
-  pianoKeys.forEach((key) => key.classList.toggle("hide"));
-};
+const handleVolume = (e) => {audio.volume = e.target.value;};
+
+const showHideKeys = () => {pianoKeys.forEach((key) => key.classList.toggle("hide"));};
 
 volumeSlider.addEventListener("input", handleVolume);
 
